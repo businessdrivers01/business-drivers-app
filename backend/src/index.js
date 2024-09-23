f3f8e44273995fd0051d0fe7ac3003172401538f
hafizasad419 <hafizasad419@gmail.com>
2024-09-23 17:47:58 +0500

    update cors policy

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ configDotenv();
 const app = express();
 app.use(cors({
     origin: ['http://localhost:5173', 'http://192.168.1.107:5173',
-        'https://www.businessdrivers01.com/', "https://businessdrivers01.com"],
+        'https://www.businessdrivers01.com', "https://businessdrivers01.com"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
